8dd19be4a4d178697e1d0f24ba9bf86f6198958f
jairsolarte-a11y <jairsolarte@unicauca.edu.co>
2026-02-28 15:44:57 -0500

    modificcion con parpadeo ejercicio 4

--- ejercicio-3.X/main.s
+++ ejercicio-3.X/main.s
@@ -7,51 +7,90 @@
 ; Inicio del progrma
 	
 ;========================================================
+; LED parpadeando con retardo
+;========================================================
 
-        #include <xc.inc>     ; libreria del el PIC 18F4550
+        #include <xc.inc>     ; Librería del compilador para el PIC
 
-        ; Configuración básica del microcontrolador
-        CONFIG FOSC   = INTOSCIO_EC    ;  Usamos el oscilador interno
-        CONFIG  WDT    = OFF           ;  Desactivamos el Watchdog (temporizador)
-        CONFIG  LVP    = OFF           ;  Desactivamos programación en bajo voltaje
-        CONFIG  PBADEN = OFF           ;  PORTB inicia como digital
-        CONFIG  MCLRE  = OFF           ;  El pin MCLR se usa como entrada digital
-        CONFIG  XINST  = OFF           ;  Desactivamos instrucciones extendidas
-        CONFIG  PWRT   = ON            ;  Activamos temporizador de encendido (más estable)
+        ; Configuración del microcontrolador
+        CONFIG  FOSC   = INTOSCIO_EC   ; Usamos oscilador interno
+        CONFIG  WDT    = OFF           ; Desactivamos Watchdog
+        CONFIG  LVP    = OFF           ; Liberamos RB5 como pin digital
+        CONFIG  PBADEN = OFF           ; PORTB inicia como digital
+        CONFIG  MCLRE  = OFF           ; MCLR como entrada digital
+        CONFIG  XINST  = OFF           ; Sin instrucciones extendidas
+        CONFIG  PWRT   = ON            ; Encendido más estable
 
-        ; Vector de reinicio
+;--------------------------------------------------------
+; Reservamos memoria para el retardo
+; Mejora: ahora usamos variables en RAM para controlar
+; el tiempo del parpadeo.
+;--------------------------------------------------------
+        PSECT udata_acs
+Delay1: DS 1        ; Primer contador 
+Delay2: DS 1        ; Segundo contador 
+
+;--------------------------------------------------------
+; Vector de reinicio
+;--------------------------------------------------------
         PSECT resetVec,class=CODE,reloc=2
-        ORG 0x00              ; Dirección inicial del programa
-        GOTO INIT             ; Saltamos a la rutina principal
+        ORG 0x00
+        GOTO INIT
 
 ;========================================================
-; Inicio del programa
+; Inicialización del sistema
 ;========================================================
 INIT:
 
-        ; Configuramos el oscilador interno a 8 MHz
-        ; Esto define la velocidad a la que trabajará el PIC
+        ; Configuramos el oscilador interno 
+        ; Esto define la velocidad de ejecución del programa
         MOVLW   0b01110010
         MOVWF   OSCCON, a
 
         ; Configuramos todos los pines como digitales
-        ; Así evitamos que funcionen como entradas analógicas
         MOVLW   0x0F
         MOVWF   ADCON1, a
 
-        ; Configuramos todo el PORTD como salida
-        ; TRISD = 0 significa salida
+        ; Configuramos todo PORTD como salida
+        ; mejora: ahora iniciamos LATD para empezar apagado
         CLRF    TRISD, a
-
-        ; Encendemos el LED conectado en RD0
-        ; Escribimos un 1 en el bit 0 del puerto D
-        MOVLW   0x01
-        MOVWF   LATD, a
+        CLRF    LATD, a
 
 ;========================================================
-; Bucle infinito
+; Mejora: ahora usamos BSF y BCF para modificar solo
+; el bit 0 sin afectar los demás pines del puerto.
 ;========================================================
-LOOP:
-        GOTO LOOP     ; El programa se queda aquí para mantener el LED encendido
+MAIN:
+
+        BSF     LATD,0,a     ; Encendemos el LED (RD0 = 1)
+        CALL    RETARDO      
+
+        BCF     LATD,0,a     ; Apagamos el LED (RD0 = 0)
+        CALL    RETARDO      
+
+        GOTO MAIN           
+
+;========================================================
+; Mejora : el tiempo se controla con
+; contadores anidados, lo que permite generar una pausa
+; visible sin usar temporizadores internos.
+;========================================================
+RETARDO:
+
+        MOVLW   200          ; Valor inicial del contador 
+        MOVWF   Delay1,a
+
+D1:
+        MOVLW   255          ; Valor inicial del contador rápido
+        MOVWF   Delay2,a
+
+D2:
+        DECFSZ  Delay2,F,a   ; disminuye Delay2
+        GOTO    D2           ; Si no es cero, sigue contando
+
+        DECFSZ  Delay1,F,a   ; Cuando Delay2 llega a 0,
+        GOTO    D1           ; disminuye Delay1 y repite
+
+        RETURN               ; Cuando los dos llegan a 0, regresa
 
         END 
