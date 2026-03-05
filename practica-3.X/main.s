ef30c70166a4962f8a8510cffd4b34f3d51e471a
Natalia Zuñiga <nataliazuniga@unicauca.edu.co>
2026-03-05 14:11:11 -0500

    Agrego practica 3

---
+++ practica-3.X/main.s
@@ -0,0 +1,9 @@
+;=========================================================
+; Código en Assembler para PIC18F4550
+; practica led sequence + boton de retroceso 
+; Frecuencia: 8 MHz (Oscilador Interno)
+;=========================================================
+ 
+    
+
+
